6beea086fc1ac541cc6ff5bb4227d3f2d9ef18b7
karimehab17 <anakarimehab67@gmail.com>
2026-09-14 21:54:46 +0300

    save 12 (Sensors & Actuators Check)

--- HAl/Sensors/Sensors_Driver.h
+++ HAl/Sensors/Sensors_Driver.h
@@ -4,10 +4,8 @@
 #include "STD_Types.h"
 
 STD_ReturnType Sensors_Init(void);
-STD_ReturnType Sensors_ReadRaw(uint16 *tempRaw,
-                              uint16 *soilRaw,
-                              uint16 *lightRaw);
-STD_ReturnType Sensors_ScaleTempC(uint16 raw, uint8_h *tempC);
-STD_ReturnType Sensors_ScalePct(uint16 raw, uint8_h *percent);
+STD_ReturnType Sensors_ReadRaw(uint16 *tempRaw, uint16 *soilRaw, uint16 *lightRaw);
+STD_ReturnType Sensors_ScaleTempC(uint16 raw, uint8 *tempC);
+STD_ReturnType Sensors_ScalePct(uint16 raw, uint8 *percent);
 
 #endif /* SENSORS_DRIVER_H */
--- src/main.c
+++ src/main.c
@@ -1,28 +1,56 @@
-#include <avr/interrupt.h>
-
 #include "STD_TYPES.h"
-#include "GPIO_interface.h"
-#include "TIMER_interface.h"
-
-ISR(TIMER0_OVF_vect)
-{
-    GPIO_SetPinValue(GPIO_PORTB, GPIO_PIN0, GPIO_HIGH);
-}
+#include "Sensors_Driver.h"
+#include "Actuators_Driver.h"
+#include <util/delay.h>
 
 int main(void)
 {
-    GPIO_SetPinDirection(GPIO_PORTB, GPIO_PIN0, GPIO_OUTPUT);
-    GPIO_SetPinValue(GPIO_PORTB, GPIO_PIN0, GPIO_LOW);
+    uint16 tempRaw = 0;
+    uint16 soilRaw = 0;
+    uint16 lightRaw = 0;
 
-    TIMER0_Init(0); // NORMAL mode
-    TIMER0_SetOverflowInterrupt(1u);
-    TIMER0_Start(5u); // Prescaler = 1024
+    uint8 temp = 0;
+    uint8 soil = 0;
+    uint8 light = 0;
 
-    sei();
+    /* Initialize Sensors + Actuators */
+    Sensors_Init();
+    ACT_Init();
 
     while (1)
     {
-    }
+        /* Read all sensors */
+        Sensors_ReadRaw(&tempRaw, &soilRaw, &lightRaw);
 
-    return 0;
+        /* Convert ADC readings */
+        Sensors_ScaleTempC(tempRaw, &temp);
+        Sensors_ScalePct(soilRaw, &soil);
+        Sensors_ScalePct(lightRaw, &light);
+
+        /* FAN: Temperature > 25°C */
+        if (temp > 25)
+            ACT_Set(ACTUATOR_FAN, ACT_STATE_ON);
+        else
+            ACT_Set(ACTUATOR_FAN, ACT_STATE_OFF);
+
+        /* PUMP: Soil < 50% */
+        if (soil < 50)
+            ACT_Set(ACTUATOR_PUMP, ACT_STATE_ON);
+        else
+            ACT_Set(ACTUATOR_PUMP, ACT_STATE_OFF);
+
+        /* LAMP: Light < 50% */
+        if (light < 50)
+            ACT_Set(ACTUATOR_LAMP, ACT_STATE_ON);
+        else
+            ACT_Set(ACTUATOR_LAMP, ACT_STATE_OFF);
+
+        /* ALARM: Temperature > 45°C */
+        if (temp > 45)
+            ACT_Set(ACTUATOR_ALARM, ACT_STATE_ON);
+        else
+            ACT_Set(ACTUATOR_ALARM, ACT_STATE_OFF);
+
+        _delay_ms(200);
+    }
 }
