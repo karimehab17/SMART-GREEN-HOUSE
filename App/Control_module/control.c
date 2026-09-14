e5eb2b779edf72fed788dfcb876ace27b1117407
karimehab17 <anakarimehab67@gmail.com>
2026-09-14 22:59:49 +0300

    (continue of Uart , Control)

--- App/Control_module/control.c
+++ App/Control_module/control.c
@@ -2,3 +2,76 @@
 #include "control.h"
 #include "Sensors_Driver.h"
 #include "STD_TYPES.h"
+
+#define TEMP_HIGH_THRESHOLD_C     35u
+#define TEMP_LOW_THRESHOLD_C      32u
+
+#define SOIL_LOW_THRESHOLD_PCT    40u
+#define SOIL_HIGH_THRESHOLD_PCT   60u
+
+#define LIGHT_LOW_THRESHOLD_PCT   25u
+#define LIGHT_HIGH_THRESHOLD_PCT  40u
+
+CONTROL_Status_t CTRL_Init(void)
+{
+    if (ACT_Init() != E_OK)
+    {
+        return CONTROL_ERROR;
+    }
+
+    if (Sensors_Init() != E_OK)
+    {
+        return CONTROL_ERROR;
+    }
+
+    return CONTROL_OK;
+}
+
+CONTROL_Status_t CTRL_Update(void)
+{
+    uint16 rawTemp = 0u;
+    uint16 rawSoil = 0u;
+    uint16 rawLight = 0u;
+
+    uint8 tempC = 0u;
+    uint8 soilPct = 0u;
+    uint8 lightPct = 0u;
+
+    if (Sensors_ReadRaw(&rawTemp, &rawSoil, &rawLight) != E_OK)
+    {
+        return CONTROL_ERROR;
+    }
+
+    (void)Sensors_ScaleTempC(rawTemp, &tempC);
+    (void)Sensors_ScalePct(rawSoil, &soilPct);
+    (void)Sensors_ScalePct(rawLight, &lightPct);
+
+ if (tempC > TEMP_HIGH_THRESHOLD_C)
+{
+    (void)ACT_Set(ACTUATOR_FAN, ACT_STATE_ON);
+}
+else if (tempC < TEMP_LOW_THRESHOLD_C)
+{
+    (void)ACT_Set(ACTUATOR_FAN, ACT_STATE_OFF);
+}
+
+if (soilPct < SOIL_LOW_THRESHOLD_PCT)
+{
+    (void)ACT_Set(ACTUATOR_PUMP, ACT_STATE_ON);
+}
+else if (soilPct > SOIL_HIGH_THRESHOLD_PCT)
+{
+    (void)ACT_Set(ACTUATOR_PUMP, ACT_STATE_OFF);
+}
+
+if (lightPct < LIGHT_LOW_THRESHOLD_PCT)
+{
+    (void)ACT_Set(ACTUATOR_LAMP, ACT_STATE_ON);
+}
+else if (lightPct > LIGHT_HIGH_THRESHOLD_PCT)
+{
+    (void)ACT_Set(ACTUATOR_LAMP, ACT_STATE_OFF);
+}
+
+    return CONTROL_OK;
+}
--- MCAL/UART/uart.c
+++ MCAL/UART/uart.c
@@ -27,7 +27,53 @@ STD_ReturnType UART_Init(uint32 Copy_u32BaudRate){
     UBRRL = (uint8)(local_u16Ubr & 0xFFU);
 
     UCSRC = (uint8)((1U << URSEL) | (1U << UCSZ1) | (1U << UCSZ0));
-    UCSRB = (uint8)(1U << RXEN) | (1U << TXEN) | (1U << RXCIE);;
+    UCSRB = (uint8)((1U << RXEN) | (1U << TXEN));
+
+    return E_OK;
+}
+
+STD_ReturnType UART_SendByte(uint8 Copy_u8Data){
+    while ((UCSRA & (1U << UDRE)) == 0U)
+    {
+    }
+
+    UDR = Copy_u8Data;
+
+    return E_OK;
+}
+
+STD_ReturnType UART_ReceiveByte(uint8 *Copy_pu8Data){
+    if (Copy_pu8Data == NULL)
+    {
+        return E_NOK;
+    }
+
+    while ((UCSRA & (1U << RXC)) == 0U)
+    {
+    }
+
+    *Copy_pu8Data = UDR;
+
+    return E_OK;
+}
+
+
+STD_ReturnType UART_SendString(const uint8 *Copy_pu8String){
+    uint8 local_u8Index = 0;
+
+    if (Copy_pu8String == NULL)
+    {
+        return E_NOK;
+    }
+
+    while (Copy_pu8String[local_u8Index] != '\0')
+    {
+        if (UART_SendByte(Copy_pu8String[local_u8Index]) != E_OK)
+        {
+            return E_NOK;
+        }
+        local_u8Index++;
+    }
 
     return E_OK;
 }
